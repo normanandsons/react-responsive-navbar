edfeec26362e3d9ecb81067366424bda32d0156d
DimitarChristoff <christoff@gmail.com>
2020-09-16 17:30:01 +0100

    initial fixes and poc

--- src/responsive-navbar.component.jsx
+++ src/responsive-navbar.component.jsx
@@ -1,50 +1,50 @@
 /* eslint-disable react/no-find-dom-node */
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { FloatingSelect } from '@opuscapita/react-floating-select';
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { debounce } from 'debounce';
 
-import './responsive-navbar.scss';
+// import './responsive-navbar.scss';
 
 export default class ResponsiveNavbar extends React.PureComponent {
   static propTypes = {
     id: PropTypes.string,
+    allowClose: PropTypes.bool,
+    onClose: PropTypes.func,
+    navRenderer: PropTypes.func,
     className: PropTypes.string,
     showNavItemBorder: PropTypes.bool,
-    showNavItemTooltip: PropTypes.bool,
-    tooltipDelay: PropTypes.number,
     fontSize: PropTypes.string,
     fontWeight: PropTypes.string,
     placeholder: PropTypes.string,
     activeKey: PropTypes.oneOfType([PropTypes.shape({}), PropTypes.number]).isRequired,
-    list: PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.node,
-      ]).isRequired,
-      href: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    })).isRequired,
+    list: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+        href: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      })
+    ).isRequired,
     onSelect: PropTypes.func,
     height: PropTypes.string,
     componentLeft: PropTypes.node,
     componentRight: PropTypes.node,
-  }
+  };
 
   static defaultProps = {
     id: 'responsive-navbar',
     className: '',
     onSelect: () => {},
+    onClose: () => {},
+    allowClose: false,
     showNavItemBorder: false,
-    showNavItemTooltip: true,
-    tooltipDelay: 2000,
     fontSize: 'inherit',
     fontWeight: 'inherit',
     placeholder: 'more...',
-    height: '40px',
+    height: 30,
     componentLeft: null,
     componentRight: null,
-  }
+  };
 
   constructor(props) {
     super(props);
@@ -64,10 +64,7 @@ export default class ResponsiveNavbar extends React.PureComponent {
 
   componentDidUpdate(prevProps, prevState) {
     // Refresh visible items if values change
-    if (
-      this.state.isSelectVisible !== prevState.isSelectVisible ||
-      this.state.lastVisibleItemIndex !== prevState.lastVisibleItemIndex
-    ) {
+    if (this.state.isSelectVisible !== prevState.isSelectVisible || this.state.lastVisibleItemIndex !== prevState.lastVisibleItemIndex) {
       this.refreshLastVisibleItem();
     }
   }
@@ -96,7 +93,7 @@ export default class ResponsiveNavbar extends React.PureComponent {
     }
 
     return lastVisible;
-  }
+  };
 
   handleResize = () => debounce(this.refreshLastVisibleItem(), 300);
 
@@ -104,38 +101,29 @@ export default class ResponsiveNavbar extends React.PureComponent {
     const lastVisibleItemIndex = this.getLastVisibleItemIndex();
     if (this.state.lastVisibleItemIndex !== lastVisibleItemIndex) {
       this.setState({
-        isSelectVisible: this.props.list.length > (lastVisibleItemIndex + 1),
+        isSelectVisible: this.props.list.length > lastVisibleItemIndex + 1,
         lastVisibleItemIndex,
       });
     }
-  }
-
-  tooltipWrapper = (node, index, tooltipContent) => {
-    const tooltip = <Tooltip id="tooltip">{tooltipContent}</Tooltip>;
-    return !this.props.showNavItemTooltip ? node :
-    <OverlayTrigger placement="bottom" key={index} overlay={tooltip} delayShow={this.props.tooltipDelay}>
-      {node}
-    </OverlayTrigger>;
-  }
+  };
 
   // Handle react-select onChange
   handleOnChange = (value) => {
     this.props.onSelect(value);
-  }
+  };
 
   // Handle navbar onClick
-  handleOnClick = href => () => {
-    this.props.onSelect(href);
-  }
+  handleOnClick = (href, index) => () => {
+    this.props.onSelect(href, index);
+  };
 
   // Render navbar item
   navbarItem = (item, index, className) => {
-    const {
-      activeKey,
-      fontWeight,
-      fontSize,
-      height,
-    } = this.props;
+    const { activeKey, fontWeight, fontSize, height, allowClose, onClose, navRenderer } = this.props;
+
+    if (navRenderer) {
+      return navRenderer(item, index, className, activeKey === index);
+    }
 
     // resolve activeKeyIndex
     let activeKeyIndex = activeKey;
@@ -144,119 +132,77 @@ export default class ResponsiveNavbar extends React.PureComponent {
     }
     return (
       <button
-        className={index === activeKeyIndex ? `${className} selected-border` : `${className}`}
+        className={classnames(className, { selected: index === activeKeyIndex, 'with-close': allowClose})}
         style={{ fontWeight, fontSize, minHeight: height }}
         id={item.id || `navItem${String(index)}`}
         key={item.id || `navitem${String(index)}`}
-        onClick={this.handleOnClick(item.href)}
+        onClick={this.handleOnClick(item.href, index)}
         ref={(r) => {
           if (r && !this.itemWidths[index]) this.itemWidths[index] = r.offsetWidth;
         }}
       >
-        <span className="responsive-navbar-item-text">{item.name}</span>
+        <span className="responsive-navbar-item-text">
+          {item.name}
+          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+          {allowClose && <i tabIndex={index + 1} role="button" className="fa fa-times" onClick={() => onClose(item.href, index)} />}
+        </span>
+
       </button>
     );
-  }
+  };
 
   doLineCount = () => {
     const { list } = this.props;
-    return list.some(item => typeof (item.name) !== 'string');
-  }
-
-  // Render navbar
-  navbar = () => {
-    const {
-      id,
-      className,
-      list,
-      showNavItemBorder,
-      height,
-    } = this.props;
-    const visibleList = this.state.lastVisibleItemIndex > -2 ?
-      list.slice(0, this.state.lastVisibleItemIndex + 1) :
-      list;
-    const itemClassName = showNavItemBorder ?
-      'responsive-navbar-item inactive-border' :
-      'responsive-navbar-item no-item-border';
-    const items = visibleList.map((item, index) => (
-      this.tooltipWrapper(this.navbarItem(item, index, itemClassName), index, item.name)
-    ));
-    const lineCount = this.doLineCount();
-    const navbarStyle = {
-      minHeight: height,
-    };
-    if (height.slice(-2) === 'px' && lineCount) {
-      const heightPx = parseInt(height.slice(0, -2), 10);
-      navbarStyle.lineHeight = `${(heightPx - 4)}px`;
-    }
-    return (
-      <div
-        id={`${id}-container`}
-        ref={(r) => { this.navbarContainerRef = r; }}
-        className={`responsive-navbar-container ${className}`}
-        style={navbarStyle}
-      >
-        {items}
-        {this.combobox()}
-        {this.componentLeft()}
-        {this.componentRight()}
-      </div>
-    );
-  }
+    return list.some((item) => typeof item.name !== 'string');
+  };
 
   resolveActiveItemFromOptions = (selectOptions) => {
     const { activeKey } = this.props;
-    let activeItem = selectOptions.find(opts => opts.value === activeKey);
+    let activeItem = selectOptions.find((opts) => opts.value === activeKey);
     if (!activeItem) {
-      activeItem = selectOptions.find(opts => opts.value === activeKey.value);
+      activeItem = selectOptions.find((opts) => opts.value === activeKey.value);
     }
     return activeItem;
-  }
+  };
 
   activeItemIndex = (activeItem) => {
     const { list } = this.props;
     if (!activeItem) return null;
-    return list.findIndex(item => item.href === activeItem.value);
-  }
+    return list.findIndex((item) => item.href === activeItem.value);
+  };
 
   // Render combobox, when all items do not fit
   combobox = () => {
-    const {
-      id,
-      list,
-      fontSize,
-      fontWeight,
-      placeholder,
-      showNavItemBorder,
-    } = this.props;
+    const { id, list, fontSize, fontWeight, placeholder, showNavItemBorder } = this.props;
     if (!this.state.isSelectVisible) {
       // return null if all nav items are visible
       return null;
     }
 
     // slice nav items list and show invisible items in the combobox
-    const navList = this.state.lastVisibleItemIndex > -2 ?
-      list.slice(this.state.lastVisibleItemIndex + 1) : list;
-    const selectOptions = navList.map(item => ({
+    const navList = this.state.lastVisibleItemIndex > -2 ? list.slice(this.state.lastVisibleItemIndex + 1) : list;
+    const selectOptions = navList.map((item) => ({
       value: item.href,
       label: item.name,
     }));
     const lineCountNeeded = this.doLineCount();
     const customLineCount = lineCountNeeded ? 'line-count' : '';
-    const customBorderClass = lineCountNeeded ? 'selected-border line-count' : 'selected-border';
-    const customInactiveBorder = lineCountNeeded ? 'inactive-border line-count' : 'inactive-border';
+    const customBorderClass = lineCountNeeded ? 'selected line-count' : 'selected';
+    const customInactiveBorder = lineCountNeeded ? 'inactive line-count' : 'inactive';
     const inactiveBorder = showNavItemBorder ? customInactiveBorder : customLineCount;
     // Resolve activeItem
     const activeItem = this.resolveActiveItemFromOptions(selectOptions);
     const activeItemIndex = this.activeItemIndex(activeItem);
-    const borderClass = activeItemIndex >= (this.state.lastVisibleItemIndex + 1) ? customBorderClass : inactiveBorder; // eslint-disable-line
+    const borderClass = activeItemIndex >= this.state.lastVisibleItemIndex + 1 ? customBorderClass : inactiveBorder; // eslint-disable-line
 
     return (
       <div
         id={`${id}-select`}
         className={`responsive-navbar-select ${borderClass}`}
         style={{ fontWeight, fontSize }}
-        ref={(r) => { this.selectContainerRef = r; }}
+        ref={(r) => {
+          this.selectContainerRef = r;
+        }}
       >
         <FloatingSelect
           name={`${id}-select-component`}
@@ -268,7 +214,7 @@ export default class ResponsiveNavbar extends React.PureComponent {
         />
       </div>
     );
-  }
+  };
 
   // Render custom left side component
   componentLeft = () => {
@@ -277,12 +223,14 @@ export default class ResponsiveNavbar extends React.PureComponent {
     return (
       <div
         className="responsive-navbar-container-left"
-        ref={(r) => { this.componentLeftContainerRef = r; }}
+        ref={(r) => {
+          this.componentLeftContainerRef = r;
+        }}
       >
-        { componentLeft }
+        {componentLeft}
       </div>
     );
-  }
+  };
 
   // Render custom right side component
   componentRight = () => {
@@ -291,14 +239,43 @@ export default class ResponsiveNavbar extends React.PureComponent {
     return (
       <div
         className="responsive-navbar-container-right"
-        ref={(r) => { this.componentRightContainerRef = r; }}
+        ref={(r) => {
+          this.componentRightContainerRef = r;
+        }}
       >
-        { componentRight }
+        {componentRight}
       </div>
     );
-  }
+  };
 
   render() {
-    return this.navbar();
+    const { id, className, list, showNavItemBorder, height } = this.props;
+    const visibleList = this.state.lastVisibleItemIndex > -2 ? list.slice(0, this.state.lastVisibleItemIndex + 1) : list;
+    const itemClassName = showNavItemBorder ? 'responsive-navbar-item inactive-border' : 'responsive-navbar-item no-item-border';
+    const items = visibleList.map((item, index) => this.navbarItem(item, index, itemClassName));
+    const lineCount = this.doLineCount();
+    const navbarStyle = {
+      minHeight: height,
+      maxHeight: height
+    };
+    if (height.slice(-2) === 'px' && lineCount) {
+      const heightPx = parseInt(height.slice(0, -2), 10);
+      navbarStyle.lineHeight = `${heightPx - 4}px`;
+    }
+    return (
+      <div
+        id={`${id}-container`}
+        ref={(r) => {
+          this.navbarContainerRef = r;
+        }}
+        className={classnames(`responsive-navbar-container`, className)}
+        style={navbarStyle}
+      >
+        {items}
+        {this.combobox()}
+        {this.componentLeft()}
+        {this.componentRight()}
+      </div>
+    );
   }
 }
--- stories/index.story.jsx
+++ stories/index.story.jsx
@@ -14,27 +14,422 @@ storiesOf('@opuscapita/react-responsive-navbar', module)
   .addDecorator(StateDecorator(store))
   .add('React Responsive NavBar', () => (state) => {
     const list = [
-      { name: 'Item 1', href: '/item1' },
-      { name: 'Item 2', href: '/item2' },
-      { name: 'A bit longer name', href: '/item3' },
-      { name: 'This is even longer one', href: '/item4' },
+      {
+        name: 'A',
+        href: 0,
+      },
+      {
+        name: 'AAL',
+        href: 1,
+      },
+      {
+        name: 'AAP',
+        href: 2,
+      },
+      {
+        name: 'AAPL',
+        href: 3,
+      },
+      {
+        name: 'ABBV',
+        href: 4,
+      },
+      {
+        name: 'ABC',
+        href: 5,
+      },
+      {
+        name: 'ABMD',
+        href: 6,
+      },
+      {
+        name: 'ABT',
+        href: 7,
+      },
+      {
+        name: 'ACN',
+        href: 8,
+      },
+      {
+        name: 'ADBE',
+        href: 9,
+      },
+      {
+        name: 'ADI',
+        href: 10,
+      },
+      {
+        name: 'ADM',
+        href: 11,
+      },
+      {
+        name: 'ADP',
+        href: 12,
+      },
+      {
+        name: 'ADS',
+        href: 13,
+      },
+      {
+        name: 'ADSK',
+        href: 14,
+      },
+      {
+        name: 'AEE',
+        href: 15,
+      },
+      {
+        name: 'AEP',
+        href: 16,
+      },
+      {
+        name: 'AES',
+        href: 17,
+      },
+      {
+        name: 'AFL',
+        href: 18,
+      },
+      {
+        name: 'AGN',
+        href: 19,
+      },
+      {
+        name: 'AIG',
+        href: 20,
+      },
+      {
+        name: 'AIV',
+        href: 21,
+      },
+      {
+        name: 'AIZ',
+        href: 22,
+      },
+      {
+        name: 'AJG',
+        href: 23,
+      },
+      {
+        name: 'AKAM',
+        href: 24,
+      },
+      {
+        name: 'ALB',
+        href: 25,
+      },
+      {
+        name: 'ALGN',
+        href: 26,
+      },
+      {
+        name: 'ALK',
+        href: 27,
+      },
+      {
+        name: 'ALL',
+        href: 28,
+      },
+      {
+        name: 'ALLE',
+        href: 29,
+      },
+      {
+        name: 'ALXN',
+        href: 30,
+      },
+      {
+        name: 'AMAT',
+        href: 31,
+      },
+      {
+        name: 'AMCR',
+        href: 32,
+      },
+      {
+        name: 'AMD',
+        href: 33,
+      },
+      {
+        name: 'AME',
+        href: 34,
+      },
+      {
+        name: 'AMGN',
+        href: 35,
+      },
+      {
+        name: 'AMP',
+        href: 36,
+      },
+      {
+        name: 'AMT',
+        href: 37,
+      },
+      {
+        name: 'AMZN',
+        href: 38,
+      },
+      {
+        name: 'ANET',
+        href: 39,
+      },
+      {
+        name: 'ANSS',
+        href: 40,
+      },
+      {
+        name: 'ANTM',
+        href: 41,
+      },
+      {
+        name: 'AON',
+        href: 42,
+      },
+      {
+        name: 'AOS',
+        href: 43,
+      },
+      {
+        name: 'APA',
+        href: 44,
+      },
+      {
+        name: 'APD',
+        href: 45,
+      },
+      {
+        name: 'APH',
+        href: 46,
+      },
+      {
+        name: 'APTV',
+        href: 47,
+      },
+      {
+        name: 'ARE',
+        href: 48,
+      },
+      {
+        name: 'ARNC',
+        href: 49,
+      },
+      {
+        name: 'ATO',
+        href: 50,
+      },
+      {
+        name: 'ATVI',
+        href: 51,
+      },
+      {
+        name: 'AVB',
+        href: 52,
+      },
+      {
+        name: 'AVGO',
+        href: 53,
+      },
+      {
+        name: 'AVY',
+        href: 54,
+      },
+      {
+        name: 'AWK',
+        href: 55,
+      },
+      {
+        name: 'AXP',
+        href: 56,
+      },
+      {
+        name: 'AZO',
+        href: 57,
+      },
+      {
+        name: 'BA',
+        href: 58,
+      },
+      {
+        name: 'BAC',
+        href: 59,
+      },
+      {
+        name: 'BAX',
+        href: 60,
+      },
+      {
+        name: 'BBY',
+        href: 61,
+      },
+      {
+        name: 'BDX',
+        href: 62,
+      },
+      {
+        name: 'BEN',
+        href: 63,
+      },
+      {
+        name: 'BF.B',
+        href: 64,
+      },
+      {
+        name: 'BIIB',
+        href: 65,
+      },
+      {
+        name: 'BK',
+        href: 66,
+      },
+      {
+        name: 'BKNG',
+        href: 67,
+      },
+      {
+        name: 'BKR',
+        href: 68,
+      },
+      {
+        name: 'BLK',
+        href: 69,
+      },
+      {
+        name: 'BLL',
+        href: 70,
+      },
+      {
+        name: 'BMY',
+        href: 71,
+      },
+      {
+        name: 'BR',
+        href: 72,
+      },
+      {
+        name: 'BRK.B',
+        href: 73,
+      },
+      {
+        name: 'BSX',
+        href: 74,
+      },
+      {
+        name: 'BWA',
+        href: 75,
+      },
+      {
+        name: 'BXP',
+        href: 76,
+      },
+      {
+        name: 'C',
+        href: 77,
+      },
+      {
+        name: 'CAG',
+        href: 78,
+      },
+      {
+        name: 'CAH',
+        href: 79,
+      },
+      {
+        name: 'CAT',
+        href: 80,
+      },
+      {
+        name: 'CB',
+        href: 81,
+      },
+      {
+        name: 'CBOE',
+        href: 82,
+      },
+      {
+        name: 'CBRE',
+        href: 83,
+      },
+      {
+        name: 'CCI',
+        href: 84,
+      },
+      {
+        name: 'CCL',
+        href: 85,
+      },
+      {
+        name: 'CDNS',
+        href: 86,
+      },
+      {
+        name: 'CDW',
+        href: 87,
+      },
+      {
+        name: 'CE',
+        href: 88,
+      },
+      {
+        name: 'CERN',
+        href: 89,
+      },
+      {
+        name: 'CF',
+        href: 90,
+      },
+      {
+        name: 'CFG',
+        href: 91,
+      },
+      {
+        name: 'CHD',
+        href: 92,
+      },
+      {
+        name: 'CHRW',
+        href: 93,
+      },
+      {
+        name: 'CHTR',
+        href: 94,
+      },
+      {
+        name: 'CI',
+        href: 95,
+      },
+      {
+        name: 'CINF',
+        href: 96,
+      },
+      {
+        name: 'CL',
+        href: 97,
+      },
+      {
+        name: 'CLX',
+        href: 98,
+      },
+      {
+        name: 'CMA',
+        href: 99,
+      },
     ];
 
     const onItemClick = (href) => {
-      const index = list.findIndex(item => item.href === href);
+      const index = list.findIndex((item) => item.href === href);
       store.set({ activeKey: index });
     };
 
     const showComponentLeft = boolean('Show component on left', false);
     const showComponentRight = boolean('Show component on right', false);
 
-    const componentLeft = showComponentLeft ?
-      <div className="demo-component">Component on left (node)</div> : null;
-    const componentRight = showComponentRight ?
-      <div className="demo-component">Component on right (node)</div> : null;
+    const componentLeft = showComponentLeft ? <div className="demo-component">Component on left (node)</div> : null;
+    const componentRight = showComponentRight ? <div className="demo-component">Component on right (node)</div> : null;
 
     const knobs = {
       list: object('List', list),
+      allowClose: boolean('Can close tabs', true),
       activeKey: state.activeKey,
       onSelect: onItemClick,
       showNavItemBorder: boolean('Show nav item border', false),
@@ -43,14 +438,8 @@ storiesOf('@opuscapita/react-responsive-navbar', module)
       fontSize: text('Font size', 'inherit'),
       fontWeight: text('Font weight', 'inherit'),
       placeholder: text('Placeholder', 'more...'),
-      height: text('Height', '40px'),
+      height: text('Height', '30'),
     };
 
-    return (
-      <NavBar
-        {...knobs}
-        componentLeft={componentLeft}
-        componentRight={componentRight}
-      />
-    );
+    return <NavBar {...knobs} componentLeft={componentLeft} componentRight={componentRight} />;
   });
